9f9b1ba10776a325f55d28c07badf8a8317c6c46
Blaine Taylor <blainetaylor@annexus.com>
2022-05-24 15:23:49 -0700

    Finished Module 2

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,18 @@
+import { data } from "../SpeakerData";
+import Speaker from "../src/components/Speaker";
+
 const IndexPage = () => {
+
     return (
-        <div>Hello From Plurasight!</div>
+        <div className="container speakers-list">
+            <div className="row">
+                {data.map(function(speaker) {
+                    return (
+                        <Speaker key={speaker.id} speaker={speaker}/>
+                    );
+                })}
+            </div>
+        </div>
     );
 }
 
